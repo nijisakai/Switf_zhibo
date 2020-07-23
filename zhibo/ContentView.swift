65d2c90ac8aa82c109d72045631c60b310f0e5c7
hy <hyinn@live.com>
2020-07-23 18:47:27 +0800

    Initial Commit

---
+++ zhibo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  zhibo
+//
+//  Created by niji on 2020/7/23.
+//  Copyright © 2020 niji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
