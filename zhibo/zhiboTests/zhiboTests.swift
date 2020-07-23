f2f418b576cd3953f134c783a71308e43a2c9057
hy <hyinn@live.com>
2020-07-23 19:00:20 +0800

    update

---
+++ zhibo/zhiboTests/zhiboTests.swift
@@ -0,0 +1,34 @@
+//
+//  zhiboTests.swift
+//  zhiboTests
+//
+//  Created by niji on 2020/7/23.
+//  Copyright © 2020 niji. All rights reserved.
+//
+
+import XCTest
+@testable import zhibo
+
+class zhiboTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
