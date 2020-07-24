847dc20b303cde25cc4442a5adaa7d33434a246c
hy <hyinn@live.com>
2020-07-24 14:15:43 +0800

    LaunchImage

---
+++ zhibo_learn/zhibo_learn/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  zhibo_learn
+//
+//  Created by niji on 2020/7/24.
+//  Copyright © 2020 niji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        Thread.sleep(forTimeInterval: 10.0)
+    }
+
+
+}
+
+
+
